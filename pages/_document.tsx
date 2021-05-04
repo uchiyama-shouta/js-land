c8d6be1d0f587bd70052f61549b3c8958073eccf
shouta <shou0604pro@icloud.com>
2021-05-04 14:29:38 +0900

    一部修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,11 +28,7 @@ export default class MyDocument extends Document {
 						}}
 					/>
 					<meta name="theme-color" content={theme.palette.primary.main} />
-					<link
-						rel="stylesheet"
-						// href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-					/>
-					<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+					<link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
 					<link rel="apple-touch-icon" href="/favicon.png" />
 				</Head>
 				<body>
