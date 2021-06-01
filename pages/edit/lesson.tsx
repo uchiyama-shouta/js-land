739dc75d257619b6dae7947422d4744e6bfca010
shouta <shou0604pro@icloud.com>
2021-06-02 03:23:55 +0900

    パフォーマンスの改善

--- pages/edit/lesson.tsx
+++ pages/edit/lesson.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
 import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
@@ -17,23 +15,8 @@ import { userState } from "../../src/store/userState";
 
 SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		card: {
-			width: 380,
-			padding: 10,
-			margin: "30px auto",
-			minHeight: 500,
-			[theme.breakpoints.down("md")]: {
-				width: 320,
-			},
-		},
-	})
-);
-
 const lesson = () => {
 	const [datas, setDatas] = useState<LessonDataType[] | []>([]);
-	const classes = useStyles();
 	const user = useRecoilValue(userState);
 
 	useEffect(() => {
@@ -42,35 +25,33 @@ const lesson = () => {
 		});
 	}, []);
 	return (
-		<>
-			<Layout>
-				{user.role === "administrator" && (
-					<div className="w-11/12 mx-auto whitespace-nowrap">
-						<h1 className="text-center my-3 text-3xl font-bold">Edit</h1>
-						<Swiper
-							spaceBetween={50}
-							slidesPerView={1}
-							pagination={{
-								type: "bullets",
-								clickable: true,
-								dynamicBullets: true,
-							}}
-							navigation
-							loop
-						>
-							{!!datas.length &&
-								datas.map((data: LessonDataType) => (
-									<SwiperSlide key={data.id}>
-										<Card className={classes.card}>
-											<LessonEditCard data={data} />
-										</Card>
-									</SwiperSlide>
-								))}
-						</Swiper>
-					</div>
-				)}
-			</Layout>
-		</>
+		<Layout>
+			{user.role === "administrator" && (
+				<div className="w-11/12 mx-auto whitespace-nowrap">
+					<h1 className="text-center my-3 text-3xl font-bold">Edit</h1>
+					<Swiper
+						spaceBetween={50}
+						slidesPerView={1}
+						pagination={{
+							type: "bullets",
+							clickable: true,
+							dynamicBullets: true,
+						}}
+						navigation
+						loop
+					>
+						{!!datas.length &&
+							datas.map((data: LessonDataType) => (
+								<SwiperSlide key={data.id}>
+									<div className="bg-white transition-shadow rounded shadow overflow-hidden w-96 mx-auto my-8 p-3">
+										<LessonEditCard data={data} />
+									</div>
+								</SwiperSlide>
+							))}
+					</Swiper>
+				</div>
+			)}
+		</Layout>
 	);
 };
 
